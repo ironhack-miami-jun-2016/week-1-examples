830f81c1ebc5b6232c9cef7507e0ab5d7f6c8a13
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-06-14 12:29:45 -0400

    Objects: Add travel method to Spaceship

--- 2-objects/app.rb
+++ 2-objects/app.rb
@@ -5,9 +5,19 @@ require_relative("lib/car.rb")
 
 first_spaceship = Spaceship.new("Woooshhh", 50)
 first_spaceship.fly
+first_spaceship.travel("Saturn")
+first_spaceship.travel("Jupiter")
+
+p first_spaceship.list_of_planets
+
 
 second_spaceship = Spaceship.new("Brrrrrrrm", 100)
 second_spaceship.fly
+second_spaceship.travel("Mars")
+second_spaceship.travel("Venus")
+second_spaceship.travel("Pluto")
+
+p second_spaceship.list_of_planets
 
 
 first_car = Car.new("Vroom")
--- 2-objects/lib/spaceship.rb
+++ 2-objects/lib/spaceship.rb
@@ -7,9 +7,19 @@ class Spaceship
   def initialize(sound, capacity)
     @sound = sound
     @capacity = capacity
+    @list_of_planets = []
   end
 
   def fly
     puts @sound
+  end
+
+  def travel(planet_name)
+    puts "Traveling to #{planet_name}"
+    @list_of_planets.push(planet_name)
+  end
+
+  def list_of_planets
+    @list_of_planets
   end
 end
