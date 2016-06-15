24ce5d588ef3bfe9f9e9a81953b9cd9a8de78b6c
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-06-15 10:05:00 -0400

    Blog: Add pagination

--- 2-blog/app.rb
+++ 2-blog/app.rb
@@ -11,5 +11,30 @@ the_blog.add_post Post.new("Second", Time.now - 3 * seconds_in_day, "Second text
 the_blog.add_post Post.new("Third", Time.now - 2 * seconds_in_day, "Third text")
 the_blog.add_post Post.new("Fourth", Time.now - 1 * seconds_in_day, "Fourth text")
 the_blog.add_post Post.new("Fifth", Time.now, "Fifth text")
+the_blog.add_post Post.new("Sixth", Time.now, "Sixth text")
+the_blog.add_post Post.new("Seventh", Time.now, "Seventh text")
+the_blog.add_post Post.new("Eighth", Time.now, "Eighth text")
+the_blog.add_post Post.new("Ninth", Time.now, "Ninth text")
+the_blog.add_post Post.new("Tenth", Time.now, "Tenth text")
+the_blog.add_post Post.new("Eleventh", Time.now, "Eleventh text")
+the_blog.add_post Post.new("Twelth", Time.now, "Twelth text")
+the_blog.add_post Post.new("Thirtheenth", Time.now, "Thirtheenth text")
+the_blog.add_post Post.new("Fourteenth", Time.now, "Fourteenth text")
 
-the_blog.publish_front_page
+the_blog.print_current_page
+
+
+input = nil
+
+while input != "exit"
+  puts "\nType [next] to see the next page or [prev] to see the previous page."
+  input = gets.chomp
+
+  if input == "next"
+    the_blog.go_next
+  elsif input == "prev"
+    the_blog.go_prev
+  end
+
+  the_blog.print_current_page
+end
