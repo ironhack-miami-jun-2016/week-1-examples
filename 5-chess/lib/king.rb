9800cd0583a3447d80cbafd453871e7eaaff9422
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-06-20 09:21:15 -0400

    Chess: Add King class

---
+++ 5-chess/lib/king.rb
@@ -0,0 +1,21 @@
+# lib/king.rb
+
+class King
+  def initialize(x, y, color)
+    @x = x
+    @y = y
+    @color = color
+  end
+
+  def can_move?(final_x, final_y)
+    change_x = (final_x - @x).abs
+    change_y = (final_y - @y).abs
+
+    if (change_x == 0 || change_x == 1) &&
+       (change_y == 0 || change_y == 1)
+      true
+    else
+      false
+    end
+  end
+end
